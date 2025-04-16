f48eb3b3b1ae781f1ee4f732dd00d37f84e20966
goalstoresults <115664503+goalstoresults@users.noreply.github.com>
2025-04-16 08:17:34 -0400

    Update summary.js

--- summary.js
+++ summary.js
@@ -28,10 +28,25 @@ document.addEventListener("DOMContentLoaded", async () => {
     const additionalLast = contact["Additional Last Name"]?.trim() || "";
     const additionalName = (additionalFirst || additionalLast) ? ` + ${additionalFirst} ${additionalLast}`.trim() : "";
 
+    const address1 = contact.address1 || "";
+    const city = contact.city || "";
+    const state = contact.state || "";
+    const postalCode = contact.postal_code || "";
+    const fullAddress = `${contact.address1 || ""}, ${contact.city || ""} ${contact.state || ""} ${contact.postal_code || ""}`.trim();
+
+
     document.getElementById("contact-full-name-display").textContent = fullName + additionalName;
     document.getElementById("contact-full-name-signature").textContent = fullName + additionalName;
     document.getElementById("field-phone").textContent = contact.phone || "";
     document.getElementById("field-email").textContent = contact.email || "";
+    document.getElementById("address").textContent = contact["Address"] || "";
+    document.getElementById("city").textContent = contact["Address"] || "";
+    document.getElementById("state").textContent = contact["Address"] || "";
+    document.getElementById("postal-code").textContent = contact["Address"] || "";
+    
+
+
+    
     document.getElementById("field-roof-size").textContent = contact["Roof Size (square footage)"] || "";
     document.getElementById("field-roof-pitch").textContent = contact["Roof pitch/slope"] || "";
     document.getElementById("field-siding-type").textContent = contact["Type of New Siding"] || "";
